44710f41bc7cec07e03a18d51bf444afb0acd649
Rishilshajan <rishilshajan1@gmail.com>
2026-01-14 17:13:58 +0530

    optimize: Corrected bugs, animations, flaws

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Resources from './pages/Resources';
-import Contact from './pages/Contact';
-import OurWork from './pages/OurWork';
-import Careers from './pages/Careers';
-import TeamMemberDetail from './pages/TeamMemberDetail';
-import OurWorkDetail from './pages/OurWorkDetail';
 import ScrollToTop from './components/ui/ScrollToTop';
+import AnimatedRoutes from './components/AnimatedRoutes';
 
 function App() {
   return (
@@ -17,18 +10,7 @@ function App() {
       <ScrollToTop />
       <div className="min-h-screen bg-white font-sans text-vic-dark">
         <Header />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-us" element={<About />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/our-work" element={<OurWork />} />
-          <Route path="/careers" element={<Careers />} />
-          <Route path="/about-us/team/:id" element={<TeamMemberDetail />} />
-          <Route path="/our-work/:id" element={<OurWorkDetail />} />
-        </Routes>
-
+        <AnimatedRoutes />
         <Footer />
       </div>
     </Router>
